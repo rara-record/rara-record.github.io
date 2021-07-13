227a437a0c6b9315505c1eff20c7cf67da61d13c
RARA <70184893+rara-record@users.noreply.github.com>
2021-07-13 17:46:08 +0900

    Update todo.js

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -58,11 +58,3 @@ function handleToDoSubmit(event) {
   saveToDos();
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
-
-
-// Todo List
-// 1. input에 내용을 입력하고 value값이 submit되면, todo list가 나타나야 한다.
-// 2. toDoList를 지울 수 있어야 한다. 
-// 3. btn을 클릭했을때, 그 btn이 속한 li만 지워야한다. (어떤 li를 지워야 하는지 알아야한다.)
-// 4. 새로고침해도 toDoList들이 사라지지 않아야한다. (localStroage) 
-
