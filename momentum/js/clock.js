4d6af8d35fb7c272570f3c011718785a0d14683c
Bora Kim <a52447879@gmail.com>
2021-07-13 23:11:03 +0900

    momentum update

---
+++ momentum/js/clock.js
@@ -0,0 +1,42 @@
+
+// setInterval
+function sayHello() {
+  console.log("hello");
+}
+//setInterval(sayHello, 5000); // 매 5초마다 호출되도록 만든다.
+
+// setTimeout
+function sayBye() {
+  console.log("bye"); 
+}
+setTimeout(sayBye, 2000); // 2초 뒤에 실행되도록 만든다
+
+
+// 시계 만드는 방법
+const clock = document.querySelector("#clock");
+const $date = document.querySelector("#date");
+  // 현재 날짜/시간을 구한다
+
+
+function getClock() {
+  // 현재의 날짜/시간을 구한다
+  const date = new Date();  
+  const year = String(date.getFullYear()).padStart(2, "0");
+  const month = String(date.getMonth()+1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  // date객체에서 현재의 시간: 분: 초를 구한다
+  $date.innerText = `${year}-${month}-${day}` 
+  clock.innerText = `${hours}:${minutes}:${seconds}`
+  console.log(`${year}-${month}-${day} ${hours}:${minutes}:${seconds}`);
+}
+getClock(); // website가 load되자마자 getClock()을 즉시 호출하고,
+//setInterval(getClock, 1000); // 또 매초마다 다시 호출
+
+
+
+
+
+
---
+++ momentum/js/todo.md
@@ -0,0 +1,13 @@
+# base
+
+## Todo List
+
+1. input에 내용을 입력하고 value값이 submit되면, todo list가 나타나야 한다.
+
+2. toDoList를 지울 수 있어야 한다. 
+
+3. btn을 클릭했을때, 그 btn이 속한 li만 지워야한다. (어떤 li를 지워야 하는지 알아야한다.)
+4. 화면과 localStroage에 추가, 삭제가 같이 이루어져야 한다.
+
+4. 새로고침해도 toDoList들이 사라지지 않아야한다.
+
