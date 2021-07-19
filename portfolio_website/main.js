5a4a3218202b6d62cbe076e0a6d29d308a754971
Bora Kim <a52447879@gmail.com>
2021-07-19 23:20:41 +0900

    JS: Navbar menu click

--- portfolio_website/main.js
+++ portfolio_website/main.js
@@ -2,7 +2,7 @@
 
 // Navbar 스크롤 내리면 background 넣기
 const navbar = document.querySelector("#navbar");
-const navbarHeight = navbar.getBoundingClientRect().height;
+const navbarHeight = navbar.clientHeight; // navbar의 높이를 구한다
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
@@ -11,3 +11,15 @@ document.addEventListener("scroll", () => {
   }
 });
 
+// navbarMenu 클릭시 target이 있는 메뉴로 이동
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event)=> {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth", block: "center"});
+})
