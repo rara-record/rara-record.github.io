bcb5e3ea14f0f784619972602d3507f6cc95895b
Bora Kim <a52447879@gmail.com>
2021-07-21 18:13:18 +0900

    JS: Contact HomeBtn

--- portfolio_website/main.js
+++ portfolio_website/main.js
@@ -16,10 +16,17 @@ const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event)=> {
   const target = event.target;
   const link = target.dataset.link;
-  if (link == null) {
-    return;
-  }
+  scrollIntoView(link);
+});
 
-  const scrollTo = document.querySelector(link);
+// contackMe 클릭시 contack으로 이동
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({behavior: "smooth", block: "center"});
-})
+}
