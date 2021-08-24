dab5f62efe78ff0d40d0fc0333ca62ff335dc178
Bora Kim <a52447879@gmail.com>
2021-08-24 20:13:05 +0900

    fix clock.js

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -28,7 +28,7 @@ function getClock() {
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
   // date객체에서 현재의 시간: 분: 초를 구한다
-  $date.innerText = `${year}-${month}-${day}` 
+  $date.innerText = `${year}-${month}-${day}`  
   clock.innerText = `${hours}:${minutes}:${seconds}`
   console.log(`${year}-${month}-${day} ${hours}:${minutes}:${seconds}`);
 }
